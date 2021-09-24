b41f85195f400f613acd87217d5d390753fd22e8
Maksims Liskins <olainerul@inbox.ru>
2021-09-24 11:29:32 +0300

    fix for gh-pages

--- src/api/requests.js
+++ src/api/requests.js
@@ -2,7 +2,7 @@ const API_KEY = "0ee6bd66c4f2ef133f03d43e9c14f56e"
 
 export default {
     async getWeather(city) {
-        const request = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`)
+        const request = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`)
         return request.json()
     }
 }
