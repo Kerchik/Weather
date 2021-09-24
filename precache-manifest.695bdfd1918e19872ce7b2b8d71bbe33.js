57c05c8e9e85784cff72b847c034d4df9b680c33
Maksims Liskins <olainerul@inbox.ru>
2021-09-24 11:22:18 +0300

    Updates

---
+++ precache-manifest.695bdfd1918e19872ce7b2b8d71bbe33.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c9a72e69dd050f769699",
+    "url": "/Weather/static/css/main.71d1b0cf.chunk.css"
+  },
+  {
+    "revision": "c9a72e69dd050f769699",
+    "url": "/Weather/static/js/main.bda7f24a.chunk.js"
+  },
+  {
+    "revision": "e6172640d3267d8b322d",
+    "url": "/Weather/static/js/runtime~main.72be58e2.js"
+  },
+  {
+    "revision": "706c5074a71b116ee84b",
+    "url": "/Weather/static/css/2.e47d4a72.chunk.css"
+  },
+  {
+    "revision": "706c5074a71b116ee84b",
+    "url": "/Weather/static/js/2.a25c3ccf.chunk.js"
+  },
+  {
+    "revision": "cc54c19b29890841267ee0750eb25a00",
+    "url": "/Weather/static/media/polygons.cc54c19b.jpg"
+  },
+  {
+    "revision": "15900e4eb03c18915e6c56b5d17bfe74",
+    "url": "/Weather/index.html"
+  }
+];
